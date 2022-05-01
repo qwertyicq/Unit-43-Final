639c883e98fa5e893f94187e14092782d589184b
Тузов Николай <tuzovns@gmail.com>
2022-05-01 13:53:10 +0300

    Initial Commit

---
+++ Unit 43 Final/Unit_43_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_43_FinalApp.swift
+//  Unit 43 Final
+//
+//  Created by Nikolay T on 01.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Unit_43_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
